16f3b27a91767d177a3f00041ca0d77f47002425
Lorman Romero <lormanrg@gmail.com>
2023-02-03 19:51:33 -0400

    divided component card in two components 🎨

--- src/front/js/pages/home.jsx
+++ src/front/js/pages/home.jsx
@@ -2,27 +2,16 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 
 import "../../styles/home.css";
-import { CategoriesCard } from "../component/CategoriesCard.jsx";
+import { Categories } from "../component/Categories.jsx";
 
 export const Home = () => {
   const { store, actions } = useContext(Context);
 
   return (
     <>
-      <div className="home">
-        <div className="container">
-          <h1>Category 1</h1>
-          <div className="home-card-list">
-            <CategoriesCard />
-          </div>
-        </div>
-        <div className="container">
-          <h1>Category 2</h1>
-          <div className="home-card-list">
-            <CategoriesCard />
-          </div>
-        </div>
-      </div>
+      <Categories />
+
+      <Categories />
     </>
   );
 };
