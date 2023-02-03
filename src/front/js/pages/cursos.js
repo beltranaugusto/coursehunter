d883449cd8bf31fcfeb19cfe381a32399710605c
Lizu-4 <liz.ugas@correo.unimet.edu.ve>
2023-02-02 21:55:52 -0400

    search bar component :building_construction:

--- src/front/js/pages/cursos.js
+++ src/front/js/pages/cursos.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
+import { SearchBar } from "../tools/searchBar";
 
 import { Context } from "../store/appContext";
 
@@ -9,46 +10,9 @@ export const Cursos = () => {
   return (
     <div className="container">
       <div className="top-part d-flex justify-content-end p-1">
-        <div className="input-group mb-3 w-50">
-          <input
-            type="text"
-            className="form-control"
-            placeholder="¿Buscas algo en particular?"
-            aria-label="Text input with dropdown button"
-          />
-          <button
-            className="btn btn-outline-secondary dropdown-toggle"
-            type="button"
-            data-bs-toggle="dropdown"
-            aria-expanded="false"
-          >
-            Filtar
-          </button>
-          <ul className="dropdown-menu dropdown-menu-end">
-            <li>
-              <a className="dropdown-item" href="#">
-                Tecnología
-                <input
-                  class="form-check-input mt-0float-end"
-                  type="checkbox"
-                  value=""
-                  aria-label="Checkbox"
-                ></input>
-              </a>
-            </li>
-            <li>
-              <a className="dropdown-item" href="#">
-                Artes culinarias
-                <input
-                  class="form-check-input mt-0 float-end"
-                  type="checkbox"
-                  value=""
-                  aria-label="Checkbox"
-                ></input>
-              </a>
-            </li>
-          </ul>
-        </div>
+        <>
+          <SearchBar />
+        </>
       </div>
       <div className="content-part">cursos aqui</div>
       <br />
