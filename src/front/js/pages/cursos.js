4cf39db2b771a626a0458e8290ed699ab9f0d016
beltranaugusto <beltranliscano@gmail.com>
2023-02-03 13:20:54 -0400

    courses page & navbar :building_construction:

--- src/front/js/pages/cursos.js
+++ src/front/js/pages/cursos.js
@@ -8,17 +8,25 @@ export const Cursos = () => {
   const { store, actions } = useContext(Context);
 
   return (
-    <div className="container">
-      <div className="top-part d-flex justify-content-end p-1">
-        <>
-          <SearchBar />
-        </>
+  <>
+    <div className="container text-center border rounded mt-5 h-100 w-100 d-flex justify-content-end flex-column p-4">
+
+      <div className="d-flex justify-content-between w-100">
+        <h3>Cursos</h3>
+        <SearchBar />
       </div>
-      <div className="content-part">cursos aqui</div>
-      <br />
-      <Link to="/">
-        <button className="btn btn-primary">Back home</button>
-      </Link>
-    </div>
+
+      {/* Lorman - Categorias */}
+      <div className="m-5"><p>Aqui van las categorias</p></div>  
+      
+
+      <div className="d-flex justify-content-end w-100">
+        <Link to="/">
+          <button className="btn btn-primary">Back home</button>
+        </Link>
+      </div>
+    
+		</div>
+  </> 
   );
 };
