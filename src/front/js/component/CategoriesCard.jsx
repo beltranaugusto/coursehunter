f38e983c3b485cf18480a5f5a229d0d759537f85
Lorman Romero <lormanrg@gmail.com>
2023-02-03 19:32:14 -0400

    added categories 🏗️

---
+++ src/front/js/component/CategoriesCard.jsx
@@ -0,0 +1,124 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
+
+export const CategoriesCard = () => {
+  return (
+    <>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="card">
+        <img
+          src="https://picsum.photos/200/200"
+          className="card-img-top"
+          alt="..."
+        />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </p>
+          <div className="div">
+            <a href="#" className="btn btn-primary">
+              Ver mas
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
