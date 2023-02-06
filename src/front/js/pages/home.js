05152ccfaf1afe30895657160dd3011bd45a21c1
Lizu-4 <liz.ugas@correo.unimet.edu.ve>
2023-02-06 17:53:46 -0400

    :pencil2: fixed typos

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -30,7 +30,7 @@ export const Home = () => {
         <div className="col-6">
           <p className="text-center bg-light text-dark  fs-5">
             Eventos <br></br>
-            <a href="#" class="btn btn-secondary">
+            <a href="#" className="btn btn-secondary">
               Visitar
             </a>
           </p>
@@ -41,68 +41,68 @@ export const Home = () => {
         </div>
         <div className="col-4 fs-5" id="categorias">
           <p className="bg-light text-dark">Categorias</p>
-          <div class="list-group">
-            <a href="#" class="list-group-item list-group-item-action">
+          <div className="list-group">
+            <a href="#" className="list-group-item list-group-item-action">
               A simple default list group item
             </a>
 
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-primary"
+              className="list-group-item list-group-item-action list-group-item-primary"
             >
               A simple primary list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-secondary"
+              className="list-group-item list-group-item-action list-group-item-secondary"
             >
               A simple secondary list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-success"
+              className="list-group-item list-group-item-action list-group-item-success"
             >
               A simple success list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-danger"
+              className="list-group-item list-group-item-action list-group-item-danger"
             >
               A simple danger list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-warning"
+              className="list-group-item list-group-item-action list-group-item-warning"
             >
               A simple warning list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-info"
+              className="list-group-item list-group-item-action list-group-item-info"
             >
               A simple info list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-light"
+              className="list-group-item list-group-item-action list-group-item-light"
             >
               A simple light list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-dark"
+              className="list-group-item list-group-item-action list-group-item-dark"
             >
               A simple dark list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-primary"
+              className="list-group-item list-group-item-action list-group-item-primary"
             >
               A simple primary list group item
             </a>
             <a
               href="#"
-              class="list-group-item list-group-item-action list-group-item-secondary"
+              className="list-group-item list-group-item-action list-group-item-secondary"
             >
               A simple secondary list group item
             </a>
