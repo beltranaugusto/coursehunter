6ba92c1e6e2db3d00402e5af3dd97fb574d9dbeb
Lizu-4 <liz.ugas@correo.unimet.edu.ve>
2023-02-28 22:01:09 -0400

    styles :lipstick:

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,50 +1,95 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { Login } from "./Login.jsx";
 import { Context } from "../store/appContext";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
-  const token = localStorage.getItem("token");
-  const { store, actions } = useContext(Context);
+    const token = localStorage.getItem("token");
+    const { store, actions } = useContext(Context);
 
-  return (
-    <nav className="navbar navbar-light bg-transparent border">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">Cursos y Eventos</span>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/cursos">
-            <button className="btn btn-primary mx-1">Cursos</button>
-          </Link>
-          <Link to="/eventos">
-            <button className="btn btn-primary mx-1">Eventos</button>
-          </Link>
-          
+    const [activeButton, setActiveButton] = useState("");
 
-          {token !== null ? (
-            <>
-              <Link to="/publicar">
-                <button className="btn btn-success mx-1">Publicar</button>
-              </Link>
-              <Link to={"/profile/" + store.user_id}>
-                <button className="btn btn-success mx-1">Mi Perfil</button>
-              </Link>
-              <button
-                className="btn btn-success mx-1"
-                onClick={() => actions.logout()}
-              >
-                Salir
-              </button>
-            </>
-          ) : (
-            <Link to="/login">
-              <button className="btn btn-secondary mx-1">Ingresar</button>
-            </Link>
-          )}
-        </div>
-      </div>
-    </nav>
-  );
+    const handleButtonClick = (buttonName) => {
+        setActiveButton(buttonName);
+    };
+
+    return (
+        <nav className="navbar navbar-light">
+            <div className="container">
+                <Link to="/">
+                    <span className="navbar-brand mb-0 h1">
+                        Cursos y Eventos
+                    </span>
+                </Link>
+                <div className="ml-auto">
+                    <Link to="/cursos">
+                        <button
+                            className={`btn btn mx-1 ${
+                                activeButton === "cursos" ? "active" : ""
+                            }`}
+                            onClick={() => handleButtonClick("cursos")}
+                        >
+                            Cursos
+                        </button>
+                    </Link>
+                    <Link to="/eventos">
+                        <button
+                            className={`btn btn mx-1 ${
+                                activeButton === "eventos" ? "active" : ""
+                            }`}
+                            onClick={() => handleButtonClick("eventos")}
+                        >
+                            Eventos
+                        </button>
+                    </Link>
+
+                    {token !== null ? (
+                        <>
+                            <Link to="/publicar">
+                                <button
+                                    className={`btn btn mx-1 ${
+                                        activeButton === "publicar"
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={() =>
+                                        handleButtonClick("publicar")
+                                    }
+                                >
+                                    Publicar
+                                </button>
+                            </Link>
+                            <Link to={"/profile/" + store.user_id}>
+                                <button
+                                    className={`btn btn mx-1 ${
+                                        activeButton === "perfil"
+                                            ? "active"
+                                            : ""
+                                    }`}
+                                    onClick={() => handleButtonClick("perfil")}
+                                >
+                                    Mi Perfil
+                                </button>
+                            </Link>
+                            <button
+                                className="btn btn mx-1 btn-log"
+                                id="log"
+                                onClick={() => actions.logout()}
+                            >
+                                Salir
+                            </button>
+                        </>
+                    ) : (
+                        <Link to="/login">
+                            <button className="btn btn mx-1 btn-log" id="log">
+                                Ingresar
+                            </button>
+                        </Link>
+                    )}
+                </div>
+            </div>
+        </nav>
+    );
 };
